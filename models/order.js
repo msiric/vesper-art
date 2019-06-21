ea4b183e2bc5ef44835962da8338f716b084a8dd
Mario <mario.siric25@gmail.com>
2019-06-22 00:12:59 +0200

    Finished episode 63

--- models/order.js
+++ models/order.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-  buyer: { type: Type.Schema.ObjectId, ref: 'User' },
-  seller: { type: Type.Schema.ObjectId, ref: 'User' },
-  gig: { type: Type.Schema.ObjectId, ref: 'Gig' },
+  buyer: { type: Schema.Types.ObjectId, ref: 'User' },
+  seller: { type: Schema.Types.ObjectId, ref: 'User' },
+  gig: { type: Schema.Types.ObjectId, ref: 'Gig' },
   messages: [
     {
       message: { type: String },
-      creator: { type: Type.Schema.ObjectId, ref: 'User' },
+      creator: { type: Schema.Types.ObjectId, ref: 'User' },
       date: { type: Date }
     }
   ],
--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const stripe = require('stripe')('sk_test_gBneokmqdbgLUsAQcHZuR4h500k4P1wiBq');
 const Gig = require('../models/gig');
+const Order = require('../models/order');
 
 const fee = 3.15;
 
@@ -19,8 +20,8 @@ router
     res.render('checkout/payment');
   })
   .post((req, res, next) => {
-    var gig = req.session.gig;
-    var price = req.session.price;
+    let gig = req.session.gig;
+    let price = req.session.price;
     price *= 100;
     stripe.customers
       .create({
@@ -40,11 +41,50 @@ router
       })
       .then(charge => {
         // New charge created on a new customer
-        res.redirect('/');
+        let order = new Order();
+        order.buyer = req.user._id;
+        order.seller = gig.owner;
+        order.gig = gig._id;
+        order.save(function(err) {
+          req.session.gig = null;
+          req.session.price = null;
+          res.redirect('/users/' + req.user._id + '/orders/' + order._id);
+        });
       })
       .catch(err => {
         // Deal with an error
       });
   });
+
+router.get('/users/:userId/orders/:orderId', (req, res, next) => {
+  req.session.orderId = req.params.orderId;
+  Order.findOne({ _id: req.params.orderId })
+    .populate('buyer')
+    .populate('seller')
+    .populate('gig')
+    .exec(function(err, order) {
+      res.render('order/order-room', { layout: 'chat_layout', order: order });
+    });
+});
+
+router.get('/users/:id/manage_orders', (req, res, next) => {
+  Order.find({ seller: req.user._id })
+    .populate('buyer')
+    .populate('seller')
+    .populate('gig')
+    .exec(function(err, order) {
+      res.render('order/order-seller', { order: order });
+    });
+});
+
+router.get('/users/:id/orders', (req, res, next) => {
+  Order.find({ buyer: req.user._id })
+    .populate('buyer')
+    .populate('seller')
+    .populate('gig')
+    .exec(function(err, order) {
+      res.render('order/order-buyer', { order: order });
+    });
+});
 
 module.exports = router;
