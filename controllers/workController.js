551370bc2348a47275cd6154e7d42b9841a54e32
Mario <mario.siric@mail.com>
2019-07-16 18:05:06 +0200

    Controller quickfix

--- controllers/workController.js
+++ controllers/workController.js
@@ -16,3 +16,7 @@ const getUserCustomWork = async (req, res, next) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+module.exports = {
+  getUserCustomWork
+};
