e0832df7247083f82c553d2d2144509beff772f7
Mario <Mario@MBP.local>
2020-03-28 14:38:02 +0100

    Changed ui framework

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { logger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import { routerMiddleware } from 'connected-react-router'
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { createHashHistory } from 'history'
+import reducers from 'redux/reducers'
+import sagas from 'redux/sagas'
+import Router from 'router'
+import Localization from 'components/LayoutComponents/Localization'
+import * as serviceWorker from './serviceWorker'
+
+// app styles
+import './global.scss'
+
+const history = createHashHistory()
+const sagaMiddleware = createSagaMiddleware()
+const routeMiddleware = routerMiddleware(history)
+const middlewares = [thunk, sagaMiddleware, routeMiddleware]
+if (process.env.NODE_ENV === 'development' && true) {
+  middlewares.push(logger)
+}
+const store = createStore(reducers(history), compose(applyMiddleware(...middlewares)))
+sagaMiddleware.run(sagas)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <Localization>
+      <Router history={history} />
+    </Localization>
+  </Provider>,
+  document.getElementById('root'),
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register()
+export { store, history }
--- wieldy/src/appRedux/actions/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Setting';
-export * from './Auth';
-export * from './Notes';
-export * from './Common';
-export * from './Contact';
