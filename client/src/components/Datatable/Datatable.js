ff020c2c23cbee323da8275dffc681e262b5bd4f
Mario Siric <mario.siric@reversinglabs.com>
2020-08-03 13:01:24 +0200

    Datatable edit

--- client/src/components/Datatable/Datatable.js
+++ client/src/components/Datatable/Datatable.js
@@ -1,136 +1,96 @@
-import _ from 'lodash';
+import React, { useState } from "react";
+import MUIDataTable from "mui-datatables";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TablePagination,
-  TableRow,
-  TableHead,
-  TableSortLabel,
+  InputLabel,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+  Select,
   Tooltip,
-} from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
+  IconButton,
+} from "@material-ui/core";
+import {
+  Add as AddIcon,
+  Edit as EditIcon,
+  Delete as DeleteIcon,
+} from "@material-ui/icons";
 
-const Datatable = ({
-  rows,
-  data,
-  sort,
-  page,
-  limit,
-  empty,
-  handleRequestSort,
-  handleRowClick,
-  handleChangePage,
-  handleChangeRowsPerPage,
-}) => {
-  const createSortHandler = (property) => (e) => {
-    handleRequestSort(e, property);
+const CustomToolbar = () => {
+  const handleAdd = () => {};
+
+  return (
+    <Tooltip title="Add artwork">
+      <IconButton onClick={handleAdd}>
+        <AddIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+const CustomToolbarSelect = ({ selectedRows }) => {
+  const handleEdit = () => {
+    console.log(
+      `edit artwork with dataIndex: ${selectedRows.data.map(
+        (row) => row.dataIndex
+      )}`
+    );
+  };
+
+  const handleDelete = () => {
+    console.log(
+      `delete artwork with dataIndexes: ${selectedRows.data.map(
+        (row) => row.dataIndex
+      )}`
+    );
+  };
+
+  const classes = {};
+
+  return (
+    <div className={classes.iconContainer}>
+      {selectedRows.data.length === 1 && (
+        <Tooltip title={"Edit artwork"}>
+          <IconButton className={classes.iconButton} onClick={handleEdit}>
+            <EditIcon className={classes.icon} />
+          </IconButton>
+        </Tooltip>
+      )}
+      <Tooltip title={"Delete artwork"}>
+        <IconButton className={classes.iconButton} onClick={handleDelete}>
+          <DeleteIcon className={classes.icon} />
+        </IconButton>
+      </Tooltip>
+    </div>
+  );
+};
+
+const Datatable = ({ columns, data }) => {
+  const [responsive, setResponsive] = useState("vertical");
+  const [tableBodyHeight, setTableBodyHeight] = useState("100%");
+  const [tableBodyMaxHeight, setTableBodyMaxHeight] = useState("");
+
+  const options = {
+    filter: false,
+    print: false,
+    download: false,
+    viewColumns: false,
+    filterType: "dropdown",
+    responsive,
+    tableBodyHeight,
+    tableBodyMaxHeight,
+    customToolbar: () => <CustomToolbar />,
+    customToolbarSelect: (selectedRows) => (
+      <CustomToolbarSelect selectedRows={selectedRows} />
+    ),
   };
 
   return (
-    <>
-      <Table className="min-w-xl" aria-labelledby="tableTitle">
-        <TableHead>
-          <TableRow className="h-64">
-            {data.length ? (
-              rows.map((row) => {
-                return (
-                  <TableCell
-                    key={row.id}
-                    align={row.align}
-                    padding={row.disablePadding ? 'none' : 'default'}
-                    /*                   sortDirection={
-                    artwork.id === row.id ? artwork.direction : false
-                  } */
-                  >
-                    {row.sort && (
-                      <Tooltip
-                        title="Sort"
-                        placement={
-                          row.align === 'right' ? 'bottom-end' : 'bottom-start'
-                        }
-                        enterDelay={300}
-                      >
-                        <TableSortLabel
-                          /*                         active={artwork.id === row.id}
-                        direction={artwork.direction} */
-                          onClick={createSortHandler(row.id)}
-                        >
-                          {row.label}
-                        </TableSortLabel>
-                      </Tooltip>
-                    )}
-                  </TableCell>
-                );
-              }, this)
-            ) : (
-              <TableCell padding={'default'}></TableCell>
-            )}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.length ? (
-            data.map((n) => {
-              return (
-                <TableRow
-                  className="h-64 dataCursor-pointer"
-                  hover
-                  role="checkbox"
-                  tabIndex={-1}
-                  key={n.id}
-                  onClick={(e) => handleRowClick(n._id)}
-                >
-                  <TableCell component="th" scope="row" align="left">
-                    <img src={n.current.cover} alt={n.name} />
-                  </TableCell>
-
-                  <TableCell component="th" scope="row" align="left">
-                    {n.current.title}
-                  </TableCell>
-
-                  <TableCell component="th" scope="row" align="right">
-                    {n.current.availability}
-                  </TableCell>
-
-                  <TableCell component="th" scope="row" align="right">
-                    {n.current.type}
-                  </TableCell>
-
-                  <TableCell component="th" scope="row" align="right">
-                    ${n.current.personal}
-                  </TableCell>
-
-                  <TableCell component="th" scope="row" align="right">
-                    ${n.current.commercial}
-                  </TableCell>
-                </TableRow>
-              );
-            })
-          ) : (
-            <TableRow tabIndex={-1}>
-              <TableCell component="th" scope="row" align="center">
-                {empty}
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <TablePagination
-        className="overflow-hidden"
-        component="div"
-        count={data.length}
-        rowsPerPage={limit}
-        page={page}
-        backIconButtonProps={{
-          'aria-label': 'Previous Page',
-        }}
-        nextIconButtonProps={{
-          'aria-label': 'Next Page',
-        }}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </>
+    <MUIDataTable
+      title={"My artwork"}
+      data={data}
+      columns={columns}
+      options={options}
+    />
   );
 };
 
--- client/src/pages/Artwork/MyArtwork.js
+++ client/src/pages/Artwork/MyArtwork.js
@@ -1,23 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import _ from 'lodash';
-import { Paper, Button, Typography, Input } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import { withRouter, useHistory } from 'react-router-dom';
-import Datatable from '../../components/Datatable/Datatable2.js';
-import { getGallery } from '../../services/artwork.js';
-import DatatableWrapper from '../../components/Datatable/DatatableWrapper.js';
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
+import { Paper, Button, Typography, Input } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { withRouter, useHistory } from "react-router-dom";
+import Datatable from "../../components/Datatable/Datatable.js";
+import { getGallery } from "../../services/artwork.js";
 
 function ProductsTable() {
   const [state, setState] = useState({
     loading: false,
     artwork: [],
-    search: '',
-    page: 0,
-    limit: 10,
-    sort: {
-      direction: 'asc',
-      id: null,
-    },
     hasMore: true,
     dataCursor: 0,
     dataCeiling: 20,
@@ -45,161 +37,36 @@ function ProductsTable() {
     fetchArtwork();
   }, []);
 
-  useEffect(() => {
-    if (state.search) {
-      setState((prevState) => ({
-        ...prevState,
-        artwork: _.filter(prevState.artwork, (item) =>
-          item.id.toLowerCase().includes(state.search.toLowerCase())
-        ),
-        page: 0,
-      }));
-    } else {
-      setState((prevState) => ({
-        ...prevState,
-        artwork: prevState.artwork,
-      }));
-    }
-  }, [state.artwork, state.search]);
-
-  function handleRequestSort(e, property) {
-    const id = property;
-    let direction = 'desc';
-
-    if (state.sort.id === property && state.sort.direction === 'desc') {
-      direction = 'asc';
-    }
-
-    setState((prevState) => ({
-      ...prevState,
-      sort: {
-        direction: direction,
-        id: id,
-      },
-    }));
-  }
-
-  function handleChangePage(e, value) {
-    setState((prevState) => ({
-      ...prevState,
-      page: value,
-    }));
-  }
-
-  function handleChangeRowsPerPage(e) {
-    setState((prevState) => ({
-      ...prevState,
-      limit: e.target.value,
-    }));
-  }
-
-  const handleSearchChange = (e, value) => {
-    setState((prevState) => ({
-      ...prevState,
-      search: value,
-    }));
-  };
-
   function handleRowClick(id) {
     history.push(`/artwork/${id}`);
   }
 
   return (
-    <>
-      <div className="flex flex-1 w-full items-center justify-between">
-        <div className="flex items-center">
-          <Typography className="hidden sm:flex mx-0 sm:mx-12" variant="h6">
-            My artwork
-          </Typography>
-        </div>
-
-        <div className="flex flex-1 items-center justify-center px-12">
-          <Paper
-            className="flex items-center w-full max-w-512 px-8 py-4 rounded-8"
-            elevation={1}
-          >
-            <Input
-              placeholder="Search"
-              className="flex flex-1 mx-8"
-              disableUnderline
-              fullWidth
-              value={state.search}
-              inputProps={{
-                'aria-label': 'Search',
-              }}
-              onChange={(e) => handleSearchChange(e)}
-            />
-          </Paper>
-        </div>
-        <Button
-          component={Link}
-          to="/add_artwork"
-          variant="contained"
-          color="secondary"
-        >
-          Add new artwork
-        </Button>
-      </div>
-      <div className="w-full flex flex-col">
-        {/* <Datatable
-          rows={[
-            {
-              id: 'cover',
-              align: 'left',
-              disablePadding: false,
-              label: 'Cover',
-              sort: true,
-            },
-            {
-              id: 'title',
-              align: 'left',
-              disablePadding: false,
-              label: 'Title',
-              sort: true,
-            },
-            {
-              id: 'availability',
-              align: 'right',
-              disablePadding: false,
-              label: 'Availability',
-              sort: true,
-            },
-            {
-              id: 'type',
-              align: 'right',
-              disablePadding: false,
-              label: 'Type',
-              sort: true,
-            },
-            {
-              id: 'personal',
-              align: 'right',
-              disablePadding: false,
-              label: 'Personal license',
-              sort: true,
-            },
-            {
-              id: 'commercial',
-              align: 'right',
-              disablePadding: false,
-              label: 'Commercial license',
-              sort: true,
-            },
-          ]}
-          data={state.artwork}
-          sort={state.sort}
-          page={state.page}
-          limit={state.limit}
-          empty={'You have no artwork'}
-          handleRequestSort={handleRequestSort}
-          handleRowClick={handleRowClick}
-          handleChangePage={handleChangePage}
-          handleChangeRowsPerPage={handleChangeRowsPerPage}
-        /> */}
-        {/* <DatatableWrapper /> */}
-        <Datatable data={state.artwork} />
-      </div>
-    </>
+    <Datatable
+      columns={[
+        {
+          name: "Cover",
+          options: {
+            customBodyRender: (value, tableMeta, updateValue) => (
+              <img src={value} />
+            ),
+          },
+        },
+        "Title",
+        "Availability",
+        "Type",
+        "Personal license",
+        "Commercial license",
+      ]}
+      data={state.artwork.map((artwork) => [
+        artwork.current.cover,
+        artwork.current.title,
+        artwork.current.availability,
+        artwork.current.type,
+        artwork.current.personal,
+        artwork.current.commercial,
+      ])}
+    />
   );
 }
 
