07cbe19d61cbaa6498b7d406d225f9a51a383ceb
msiric <mario.siric@mail.com>
2020-09-18 15:48:36 +0200

    Quickfix

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -45,86 +45,89 @@ export const postDownload = async ({
       if (licensePrice === 0) {
         // $TODO Treba li dohvacat usera?
         const foundUser = await fetchUserById({ userId, session });
-        if (foundUser && foundUser.active) {
-          const { licenseError } = licenseValidator(
-            sanitizeData({
-              licenseOwner: foundUser._id,
-              licenseArtwork: foundVersion.artwork._id,
-              licenseAssignee,
-              licenseCompany,
-              licenseType,
-              licensePrice,
-            })
-          );
-          if (licenseError) throw createError(400, licenseError);
-          const newLicense = new License();
-          newLicense.owner = foundUser._id;
-          newLicense.artwork = foundVersion.artwork._id;
-          newLicense.fingerprint = crypto.randomBytes(20).toString('hex');
-          newLicense.assignee = licenseAssignee;
-          newLicense.company = licenseCompany;
-          newLicense.type = licenseType;
-          newLicense.active = true;
-          newLicense.price = licensePrice;
-          const savedLicense = await newLicense.save({ session });
-          const { orderError } = orderValidator(
-            sanitizeData({
-              orderBuyer: foundUser._id,
-              orderSeller: foundVersion.artwork.owner._id,
-              orderArtwork: foundVersion.artwork._id,
-              orderVersion: foundVersion._id,
-              orderDiscount: null,
-              orderLicense: savedLicense._id,
-              orderSpent: 0,
-              orderEarned: 0,
-              orderFee: 0,
-              orderIntent: null,
-            })
-          );
-          if (orderError) throw createError(400, orderError);
-          const orderObject = {
-            buyerId: foundUser._id,
-            sellerId: foundVersion.artwork.owner._id,
-            artworkId: foundVersion.artwork._id,
-            versionId: foundVersion._id,
-            discountId: null,
-            licenseId: savedLicense._id,
-            review: null,
-            spent: 0,
-            earned: 0,
-            fee: 0,
-            status: 'completed',
-            intentId: null,
-          };
-          const savedOrder = await addNewOrder({
-            orderData: orderObject,
-            session,
-          });
-          await editUserPurchase({
-            userId: foundUser._id,
-            orderId: savedOrder._id,
-            session,
-          });
-          await editUserSale({
-            userId: foundVersion.artwork.owner._id,
-            orderId: savedOrder._id,
-            session,
-          });
-          // new start
-          await addNewNotification({
-            notificationLink: savedOrder._id,
-            notificationType: 'order',
-            notificationReceiver: foundVersion.artwork.owner._id,
-            session,
-          });
-          socketApi.sendNotification(
-            foundVersion.artwork.owner._id,
-            savedOrder._id
-          );
-          // new end
-          return { message: 'Order completed successfully' };
+        if (foundVersion.artwork.owner._id !== foundUser._id) {
+          if (foundUser && foundUser.active) {
+            const { licenseError } = licenseValidator(
+              sanitizeData({
+                licenseOwner: foundUser._id,
+                licenseArtwork: foundVersion.artwork._id,
+                licenseAssignee,
+                licenseCompany,
+                licenseType,
+                licensePrice,
+              })
+            );
+            if (licenseError) throw createError(400, licenseError);
+            const newLicense = new License();
+            newLicense.owner = foundUser._id;
+            newLicense.artwork = foundVersion.artwork._id;
+            newLicense.fingerprint = crypto.randomBytes(20).toString('hex');
+            newLicense.assignee = licenseAssignee;
+            newLicense.company = licenseCompany;
+            newLicense.type = licenseType;
+            newLicense.active = true;
+            newLicense.price = licensePrice;
+            const savedLicense = await newLicense.save({ session });
+            const { orderError } = orderValidator(
+              sanitizeData({
+                orderBuyer: foundUser._id,
+                orderSeller: foundVersion.artwork.owner._id,
+                orderArtwork: foundVersion.artwork._id,
+                orderVersion: foundVersion._id,
+                orderDiscount: null,
+                orderLicense: savedLicense._id,
+                orderSpent: 0,
+                orderEarned: 0,
+                orderFee: 0,
+                orderIntent: null,
+              })
+            );
+            if (orderError) throw createError(400, orderError);
+            const orderObject = {
+              buyerId: foundUser._id,
+              sellerId: foundVersion.artwork.owner._id,
+              artworkId: foundVersion.artwork._id,
+              versionId: foundVersion._id,
+              discountId: null,
+              licenseId: savedLicense._id,
+              review: null,
+              spent: 0,
+              earned: 0,
+              fee: 0,
+              status: 'completed',
+              intentId: null,
+            };
+            const savedOrder = await addNewOrder({
+              orderData: orderObject,
+              session,
+            });
+            await editUserPurchase({
+              userId: foundUser._id,
+              orderId: savedOrder._id,
+              session,
+            });
+            await editUserSale({
+              userId: foundVersion.artwork.owner._id,
+              orderId: savedOrder._id,
+              session,
+            });
+            // new start
+            await addNewNotification({
+              notificationLink: savedOrder._id,
+              notificationType: 'order',
+              notificationReceiver: foundVersion.artwork.owner._id,
+              session,
+            });
+            socketApi.sendNotification(
+              foundVersion.artwork.owner._id,
+              savedOrder._id
+            );
+            // new end
+            return { message: 'Order completed successfully' };
+          }
+          throw createError(400, 'User not found');
         }
-        throw createError(400, 'User not found');
+        throw createError(400, 'You are the owner of this artwork');
       }
       throw createError(400, 'This artwork is not free');
     }
