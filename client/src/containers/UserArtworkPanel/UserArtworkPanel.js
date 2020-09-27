1e6263acedbba464c5f9558f6496e157d4cadc3d
Mario Siric <msiric@localhost.localdomain>
2020-09-27 18:31:53 +0200

    Fix saved artwork toggle

--- client/src/containers/UserArtworkPanel/UserArtworkPanel.js
+++ client/src/containers/UserArtworkPanel/UserArtworkPanel.js
@@ -1,19 +1,19 @@
-import { Grid, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
-import SwipeCard from '../../components/SwipeCard/SwipeCard.js';
-import ArtworkPanel from '../../containers/ArtworkPanel/ArtworkPanel.js';
+import { Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import SwipeCard from "../../components/SwipeCard/SwipeCard.js";
+import ArtworkPanel from "../../containers/ArtworkPanel/ArtworkPanel.js";
 
 const useStyles = makeStyles({
   paper: {
-    height: '100%',
+    height: "100%",
   },
   profileArtworkContainer: {
-    '&> div': {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
+    "&> div": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100%",
     },
   },
 });
@@ -35,60 +35,13 @@ const UserArtworkPanel = ({
           <SwipeCard
             tabs={{
               value: tabs.value,
-              hasMore: '',
-              type: 'artwork',
+              hasMore: "",
+              type: "artwork",
               headings: [
-                { display: true, label: 'User artwork', props: {} },
-                { display: true, label: 'Saved artwork', props: {} },
-              ],
-              items: [
-                {
-                  display: true,
-                  iterable: true,
-                  content: user.artwork.length,
-                  component: (
-                    <ArtworkPanel
-                      elements={user.artwork}
-                      hasMore={null}
-                      loadMore={loadMoreArtwork}
-                      type="artwork"
-                    />
-                  ),
-                  error: 'You have no artwork to display',
-                  loading: false,
-                },
-                {
-                  display: true,
-                  iterable: true,
-                  content: user.savedArtwork.length,
-                  component: (
-                    <ArtworkPanel
-                      elements={user.savedArtwork}
-                      hasMore={null}
-                      loadMore={loadMoreSaves}
-                      type="artwork"
-                    />
-                  ),
-                  error: 'You have no saved artwork',
-                  loading: tabs.loading,
-                },
-              ],
-            }}
-            handleTabsChange={handleTabsChange}
-            handleChangeIndex={handleChangeIndex}
-          />
-        </Paper>
-      ) : (
-        <Paper className={classes.artwork}>
-          <SwipeCard
-            tabs={{
-              value: tabs.value,
-              hasMore: '',
-              headings: [
-                { display: true, label: 'User artwork', props: {} },
+                { display: true, label: "User artwork", props: {} },
                 {
                   display: user.displaySaves,
-                  label: 'Saved artwork',
+                  label: "Saved artwork",
                   props: {},
                 },
               ],
@@ -105,7 +58,7 @@ const UserArtworkPanel = ({
                       type="artwork"
                     />
                   ),
-                  error: 'This user has no artwork to display',
+                  error: "You have no artwork to display",
                   loading: false,
                 },
                 {
@@ -120,7 +73,58 @@ const UserArtworkPanel = ({
                       type="artwork"
                     />
                   ),
-                  error: 'This user has no saved artwork',
+                  error: "You have no saved artwork",
+                  loading: tabs.loading,
+                },
+              ],
+            }}
+            handleTabsChange={handleTabsChange}
+            handleChangeIndex={handleChangeIndex}
+          />
+        </Paper>
+      ) : (
+        <Paper className={classes.artwork}>
+          <SwipeCard
+            tabs={{
+              value: tabs.value,
+              hasMore: "",
+              headings: [
+                { display: true, label: "User artwork", props: {} },
+                {
+                  display: user.displaySaves,
+                  label: "Saved artwork",
+                  props: {},
+                },
+              ],
+              items: [
+                {
+                  display: true,
+                  iterable: true,
+                  content: user.artwork.length,
+                  component: (
+                    <ArtworkPanel
+                      elements={user.artwork}
+                      hasMore={null}
+                      loadMore={loadMoreArtwork}
+                      type="artwork"
+                    />
+                  ),
+                  error: "This user has no artwork to display",
+                  loading: false,
+                },
+                {
+                  display: user.displaySaves,
+                  iterable: true,
+                  content: user.savedArtwork.length,
+                  component: (
+                    <ArtworkPanel
+                      elements={user.savedArtwork}
+                      hasMore={null}
+                      loadMore={loadMoreSaves}
+                      type="artwork"
+                    />
+                  ),
+                  error: "This user has no saved artwork",
                   loading: tabs.loading,
                 },
               ],
