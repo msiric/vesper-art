d529474e3e0517c610741dbba934e2c9986470d2
Mario Siric <mario.siric@mail.com>
2020-11-03 20:52:47 +0100

    Testing new footer

--- client/src/containers/Footer/Footer.js
+++ client/src/containers/Footer/Footer.js
@@ -1,31 +1,99 @@
-import { Container, Link, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Box, Container, Grid, Link, Typography } from "@material-ui/core";
 import React from "react";
-
-const useStyles = makeStyles((muiTheme) => ({
-  footerContainer: {
-    zIndex: 1000,
-    padding: muiTheme.spacing(3, 2),
-    marginTop: "auto",
-    backgroundColor: muiTheme.palette.grey[800],
-    textAlign: "center",
-  },
-}));
+import footerStyles from "./styles.js";
 
 const Footer = () => {
-  const classes = useStyles();
+  const classes = footerStyles();
 
   return (
+    // old
+    // <footer className={classes.footerContainer}>
+    //   <Container>
+    //     <Typography variant="body2" color="textSecondary">
+    //       {"Copyright © "}
+    //       <Link color="inherit" href="https://material-ui.com/">
+    //         Material UI
+    //       </Link>{" "}
+    //       {new Date().getFullYear()}
+    //       {"."}
+    //     </Typography>
+    //   </Container>
+    // </footer>
     <footer className={classes.footerContainer}>
       <Container>
-        <Typography variant="body2" color="textSecondary">
-          {"Copyright © "}
-          <Link color="inherit" href="https://material-ui.com/">
-            Material UI
-          </Link>{" "}
-          {new Date().getFullYear()}
-          {"."}
-        </Typography>
+        <Grid container spacing={2} className={classes.footerNav}>
+          <Grid item xs={10} sm={6} md={3}>
+            <Box className={classes.footerHeading}>Who we are</Box>
+            <ul>
+              <li>
+                <Link to={""}>Home</Link>
+              </li>
+              <li>
+                <Link to={""}>About us</Link>
+              </li>
+              <li>
+                <Link to={""}>How it works</Link>
+              </li>
+            </ul>
+          </Grid>
+          <Grid item xs={10} sm={6} md={3}>
+            <Box className={classes.footerHeading}>Community</Box>
+            <ul>
+              <li>
+                <Link to={""}>Selling on "test"</Link>
+              </li>
+              <li>
+                <Link to={""}>Buying on "test"</Link>
+              </li>
+              <li>
+                <Link to={""}>Trust & safety</Link>
+              </li>
+            </ul>
+          </Grid>
+          <Grid item xs={10} sm={6} md={3}>
+            <Box className={classes.footerHeading}>Support</Box>
+            <ul>
+              <li>
+                <Link to={""}>FAQ &amp; Support</Link>
+              </li>
+              <li>
+                <Link to={""}>Contact Us</Link>
+              </li>
+            </ul>
+          </Grid>
+          <Grid item xs={10} sm={6} md={3}>
+            <Box className={classes.footerHeading}>Connect</Box>
+            <ul>
+              <li>
+                <a
+                  className={classes.socialIcon}
+                  href="https://www.facebook.com/hifionafinance/"
+                  target="_blank"
+                >
+                  {/* <Facebook /> */}
+                </a>
+                <a
+                  className={classes.socialIcon}
+                  href="https://twitter.com/hifionafinance"
+                  target="_blank"
+                >
+                  {/* <Twitter /> */}
+                </a>
+              </li>
+            </ul>
+          </Grid>
+        </Grid>
+        <hr className={classes.hrLight} />
+        <Grid container className={classes.disclaimers}>
+          <Grid item xs={10} sm={6} className={classes.copyright}>
+            <Typography>diagon</Typography>
+            <span>Copyright &copy; {new Date().getFullYear()}</span>
+          </Grid>
+          <Grid item xs={10} sm={6} className={classes.disclosures}>
+            <Link to={""}>Privacy Policy</Link>{" "}
+            <Link to={""}>Terms of Service</Link>{" "}
+          </Grid>
+        </Grid>
       </Container>
     </footer>
   );
---
+++ client/src/containers/Footer/styles.js
@@ -0,0 +1,5 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const footerStyles = makeStyles((muiTheme) => ({}));
+
+export default footerStyles;
