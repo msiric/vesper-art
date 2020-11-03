2c3856dad31f0659833a80bc554d6f51c608d9af
unknown <mario.siric@mail.com>
2020-11-04 00:44:28 +0100

    Add tags to version model

--- models/version.js
+++ models/version.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
-import mongooseDeepPopulate from 'mongoose-deep-populate';
-import fuzzySearch from 'mongoose-fuzzy-searching';
-import { formatAmount } from '../common/helpers.js';
+import mongoose from "mongoose";
+import mongooseDeepPopulate from "mongoose-deep-populate";
+import fuzzySearch from "mongoose-fuzzy-searching";
+import { formatAmount } from "../common/helpers.js";
 
 const deepPopulate = mongooseDeepPopulate(mongoose);
 
 const Schema = mongoose.Schema;
 
 const VersionSchema = new Schema({
-  artwork: { type: Schema.Types.ObjectId, ref: 'Artwork' }, // nesting
+  artwork: { type: Schema.Types.ObjectId, ref: "Artwork" }, // nesting
   orientation: String,
   dominant: String,
   height: String,
@@ -16,30 +16,31 @@ const VersionSchema = new Schema({
   type: String,
   title: String,
   category: String,
+  tags: [],
   description: String,
   license: String,
   use: String,
   personal: { type: Number, get: formatAmount },
   commercial: { type: Number, get: formatAmount },
   availability: String,
-  cover: { type: String, default: 'http://placehold.it/350x150' },
+  cover: { type: String, default: "http://placehold.it/350x150" },
   media: String,
   created: { type: Date, default: Date.now },
 });
 
-VersionSchema.set('toObject', { getters: true });
-VersionSchema.set('toJSON', { getters: true });
+VersionSchema.set("toObject", { getters: true });
+VersionSchema.set("toJSON", { getters: true });
 
 VersionSchema.plugin(deepPopulate);
 
 VersionSchema.plugin(fuzzySearch, {
   fields: [
-    { name: 'title', weight: 3 },
-    { name: 'description', weight: 1 },
+    { name: "title", weight: 3 },
+    { name: "description", weight: 1 },
   ],
 });
 
-const Version = mongoose.model('Version', VersionSchema);
+const Version = mongoose.model("Version", VersionSchema);
 
 Version.createCollection();
 
