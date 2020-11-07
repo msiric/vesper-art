b79d36882e4880c76c611f2fb398c0643ef0cf27
Mario Siric <msiric@localhost.localdomain>
2020-11-07 03:03:30 +0100

    Edits

--- client/src/pages/Home/HowItWorks.js
+++ client/src/pages/Home/HowItWorks.js
@@ -18,59 +18,77 @@ import globalStyles from "../../styles/global.js";
 const SELLER_ILLUSTRATIONS = [
   {
     heading: "Unleash your creativity",
-    paragraph: "",
+    paragraph:
+      "Set up your profile and work at your own pace. If and when you're ready, go through the onboarding process and you can start earning money from your art. It's that simple.",
     illustration: <WorkingArtist />,
   },
   {
     heading: "Upload for everyone to see ",
-    paragraph: "",
+    paragraph:
+      "Share your artwork on the platform for others to enjoy while never having to worry about it being stolen or used without your permission.",
     illustration: <UploadArtwork />,
   },
   {
     heading: "You're in charge",
     paragraph:
-      "Fill out the information about your artwork and the licenses you will be offering. It's all up to you.",
+      "Fill out the information about your artwork and the licenses you will be offering. You have full control and freedom to adjust everything to your liking.",
     illustration: <MakeAdjustments />,
   },
   {
     heading: "Stay up to date",
     paragraph:
-      "Every time someone comments, downloads, purchases or leaves a rating on your artwork, you will be notified",
+      "Every time someone comments, downloads, purchases or leaves a rating on your artwork, you will be notified. You'll never miss a thing.",
     illustration: <ReceiveUpdates />,
   },
   {
     heading: "Track your progress",
-    paragraph: "Visit your dashboard to  ",
+    paragraph:
+      "Visit your dashboard to monitor every sale you made, every license you issued and every event that happened at a given time.",
     illustration: <MonitorDashboard />,
   },
   {
     heading: "Get payed for your work",
-    paragraph: "",
+    paragraph:
+      "All the transactions are securely processed by Stripe and reliably kept in your account balance for you to transfer to a destination of your choice.",
     illustration: <EarnMoney />,
   },
 ];
 
 const BUYER_ILLUSTRATIONS = [
-  { heading: "Browse artwork", paragraph: "", illustration: <BrowseArtwork /> },
   {
-    heading: "Pick something you like",
-    paragraph: "",
+    heading: "Browse artwork",
+    paragraph:
+      "Sign up and browse the diverse collection of artwork from our talented artists. There's something for everyone.",
+    illustration: <BrowseArtwork />,
+  },
+  {
+    heading: "Select art of your choosing",
+    paragraph:
+      "Once you find something you like, favorite it, comment on it and explore more from that artist. Interact and stay in touch.",
     illustration: <SelectArtwork />,
   },
-  { heading: "Make it yours", paragraph: "", illustration: <StripePayment /> },
+  {
+    heading: "Make it yours",
+    paragraph:
+      "Once you have your eyes on a specific piece, choose a license and download/purchase it, depending on what the artist is offering.",
+    illustration: <StripePayment />,
+  },
   {
     heading: "Keep it yours",
-    paragraph: "",
+    paragraph:
+      "Every download/purchase comes with a license that proves your ownership of the artwork and the order that was placed to acquire it.",
     illustration: <LicenseCertification />,
   },
   {
     heading: "Admire your collection",
-    paragraph: "",
+    paragraph:
+      "Head to your gallery at any time to enjoy the art that you own and play the slideshow in fullscreen mode for complete immersion.",
     illustration: <ArtworkGallery />,
   },
   {
     heading: "Share it, show it, love it",
-    paragraph: "",
+    paragraph:
+      "You can download your artwork on as many devices as you want and access it at anytime from anywhere, knowing that your ownership cannot be disputed.",
     illustration: <DisplayCollection />,
   },
 ];
