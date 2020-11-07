3181b2df8c31b1f4931f3491cd6118dce6b732b5
unknown <mario.siric@mail.com>
2020-11-08 00:17:00 +0100

    Add selling page text

--- client/src/pages/Community/Buying.js
+++ client/src/pages/Community/Buying.js
@@ -1,6 +1,6 @@
-import { Button, Card, Container, Grid, Typography } from "@material-ui/core";
+import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
-import { app } from "../../../../common/constants.js";
+import { app, payment } from "../../../../common/constants.js";
 import MainHeading from "../../components/MainHeading";
 import globalStyles from "../../styles/global.js";
 
@@ -12,29 +12,34 @@ const Buying = () => {
       <Grid container spacing={2}>
         <Grid item sm={12}>
           <MainHeading text={`Buying on ${app.name}`} />
+          <Typography>Signing Up</Typography>
           <Typography>
-            {`Get up to speed with how ${app.name} works in six steps from both the
-            seller's and buyer's perspective.`}
+            {`Signing up is free and required for users to start selling on 
+            ${app.name}. When you sign up, you will be able to download, 
+            purchase and upload your own artwork. 
+            Your art can be published as preview only (users can only view without 
+            downloading or purchasing), commercially available or free to download.
+            Additionally, you have complete control over the art you are publishing, 
+            including the types of licenses you want to include and how much 
+            to charge for each type.
+            In order to monetize your art, you will have to go through the 
+            onboarding process where you will be taken to Stripe to finalize 
+            your account setup and receive future payments.
+            Whenever an order is placed, commercial or free, the buyer will receive
+            a license for using your artwork in compliance with the license's permissions
+            along with the actual artwork.
+            Every license that was ever generated can be verified using the platform's 
+            verifer system to ensure that your art is never stolen or used in a way that 
+            is outside the scope of what the license permits.
+            This way, both you and the licensee are protected.
+            When a commercial order is placed, you as a seller will receive 
+            ${(1 - payment.appFee) * 100}% of the total order value.
+            For example, if you price your artwork at $100 for personal or commercial usage, 
+            you will receive $${
+              (1 - payment.appFee) * 100
+            } once your artwork has been purchased. 
+            For more information, see the Terms of Service.`}
           </Typography>
-        </Grid>
-        <Grid item sm={12} md={6}>
-          <Card>
-            <MainHeading text="Sellers" style={{ textAlign: "center" }} />
-          </Card>
-        </Grid>
-        <Grid item sm={12} md={6}>
-          <Card>
-            <MainHeading text="Buyers" style={{ textAlign: "center" }} />
-          </Card>
-        </Grid>
-        <Grid item sm={12}>
-          <Typography>Join the platform and get started</Typography>
-          <Button variant="outlined">Sign up</Button>
-          <Typography>
-            Want to learn more? Click on one of the pages below
-          </Typography>
-          <Button variant="outlined">{`Selling on ${app.name}`}</Button>
-          <Button variant="outlined">{`Buying on ${app.name}`}</Button>
         </Grid>
       </Grid>
     </Container>
--- client/src/pages/Community/Selling.js
+++ client/src/pages/Community/Selling.js
@@ -1,6 +1,6 @@
 import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
-import { app } from "../../../../common/constants.js";
+import { app, payment } from "../../../../common/constants.js";
 import MainHeading from "../../components/MainHeading";
 import globalStyles from "../../styles/global.js";
 
@@ -12,9 +12,40 @@ const Selling = () => {
       <Grid container spacing={2}>
         <Grid item sm={12}>
           <MainHeading text={`Selling on ${app.name}`} />
+          <Typography>Signing Up</Typography>
           <Typography>
-            {`Get up to speed with how ${app.name} works in six steps from both the
-            seller's and buyer's perspective.`}
+            {`Signing up is free and required for users to start selling on 
+            ${app.name}. When you sign up, you will be able to download, 
+            purchase and upload your own artwork. 
+            Your art can be published as preview only, which allows users to only view your 
+            artwork without downloading or purchasing, or available for download, which allows 
+            users to download your artwork for free or purchase it, depending on your preferences.
+            Additionally, you have complete control over the art you are publishing, 
+            including the types of licenses you want to include and how much 
+            to charge for each type.
+            If you choose to publish your artwork as available for download, you may offer
+            a personal license, which allows the user to $TODO include personal license permissions,
+            and/or a commercial license, which allows the user to $TODO include commercial license 
+            permissions.
+            In order to monetize your art, you will have to go through the 
+            onboarding process where you will be taken to Stripe to finalize 
+            your account setup and receive future payments.
+            Whenever an order is placed, commercial or free, the buyer will receive
+            a license for using your artwork in compliance with the license's permissions
+            along with the actual artwork.
+            Every license that was ever generated can be verified using the platform's 
+            verifer system to ensure that your art is never stolen or used in a way that 
+            is outside the scope of what the license permits.
+            This way, both you and the licensee are protected.
+            When a commercial order is placed, you as a seller will receive 
+            ${(1 - payment.appFee) * 100}% of the total order value.
+            For example, if you price your artwork at $100 for personal or commercial usage, 
+            you will receive $${
+              (1 - payment.appFee) * 100
+            } once your artwork has been purchased.
+            Your funds will appear in your Stripe account after 7 days (due to processing and 
+            handling disputes).
+            For more information, see the Terms of Service.`}
           </Typography>
         </Grid>
       </Grid>
