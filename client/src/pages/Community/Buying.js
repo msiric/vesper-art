0f577c64053f469ff1ff6b7c8b08515405490f74
unknown <mario.siric@mail.com>
2020-11-08 16:23:54 +0100

    Edit buying page

--- client/src/pages/Community/Buying.js
+++ client/src/pages/Community/Buying.js
@@ -1,6 +1,6 @@
 import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
-import { app, payment } from "../../../../common/constants.js";
+import { app } from "../../../../common/constants.js";
 import MainHeading from "../../components/MainHeading";
 import globalStyles from "../../styles/global.js";
 
@@ -17,27 +17,25 @@ const Buying = () => {
             {`Signing up is free and required for users to start downloading and purchasing
             art on ${app.name}. When you sign up, you will be able to download, 
             purchase and upload your own artwork. 
-            Your art can be published as preview only (users can only view without 
-            downloading or purchasing), commercially available or free to download.
-            Additionally, you have complete control over the art you are publishing, 
-            including the types of licenses you want to include and how much 
-            to charge for each type.
-            In order to monetize your art, you will have to go through the 
-            onboarding process where you will be taken to Stripe to finalize 
-            your account setup and receive future payments.
-            Whenever an order is placed, commercial or free, the buyer will receive
-            a license for using your artwork in compliance with the license's permissions
-            along with the actual artwork.
-            Every license that was ever generated can be verified using the platform's 
-            verifer system to ensure that your art is never stolen or used in a way that 
-            is outside the scope of what the license permits.
-            This way, both you and the licensee are protected.
-            When a commercial order is placed, you as a seller will receive 
-            ${(1 - payment.appFee) * 100}% of the total order value.
-            For example, if you price your artwork at $100 for personal or commercial usage, 
-            you will receive $${
-              (1 - payment.appFee) * 100
-            } once your artwork has been purchased. 
+            When browsing artwork, pay attention to the their type and availability.
+            Artists can upload art as preview only, which allows you to only view the 
+            artwork without downloading or purchasing, or available for download, which allows 
+            you to download the artwork for free or purchase it, depending on the type of the artwork.
+            If an artwork is published as available for download, the artist may offer
+            a personal license, which allows you to $TODO include personal license permissions,
+            and/or a commercial license, which allows you to $TODO include commercial license 
+            permissions.
+            A license may be free of charge which will allow you to download the artwork
+            for free compared to a billable license, which you will have to purchase.
+            In both cases, you will have to fill out the necessary license information in order to
+            receive both the artwork you picked and the license along with it.
+            This allows you to prove that you are the legitimate owner of the artwork and
+            that you can use it as specified in the license's permissions, so make sure you
+            store it somewhere safe.
+            In addition to the above, you can verify your licenses using the platform's verifier
+            system to ensure the authenticity and non-repudiation of your ownership.
+            Every billable order you place will be subject to the ${app.name}'s service fee, which
+            will be displayed on the checkout page before you confirm your payment.
             For more information, see the Terms of Service.`}
           </Typography>
         </Grid>
