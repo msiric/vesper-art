c4cd50bd8d1840c804f9fc83f9864f910230b475
Mario Siric <mario.siric@mail.com>
2020-11-09 01:19:58 +0100

    Edit homepage banner

---
+++ client/src/containers/HomeBanner/index.js
@@ -0,0 +1,123 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import React, { useContext } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { UserContext } from "../../contexts/User.js";
+import homeBannerStyles from "./styles";
+
+const HomeBanner = () => {
+  const [userStore] = useContext(UserContext);
+
+  const classes = homeBannerStyles();
+
+  return [
+    <Grid item xs={12} md={9}>
+      <Card className={classes.bannerContainer}>
+        <CardContent
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            zIndex: 1,
+          }}
+        >
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              marginBottom: 16,
+              padding: 16,
+            }}
+          >
+            <Typography
+              style={{
+                textAlign: "center",
+                fontSize: 24,
+                fontWeight: "bold",
+                width: "60%",
+              }}
+            >
+              Browse, share and collect digital art the way it's supposed to be
+              done
+            </Typography>
+          </Box>
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            {!userStore.authenticated && (
+              <Button component={RouterLink} to="/signup">
+                Sign up
+              </Button>
+            )}
+            <Button component={RouterLink} to="/how_it_works">
+              How it works
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Grid>,
+    <Grid item xs={12} md={3}>
+      <Card
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <CardContent
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              marginBottom: 16,
+              padding: 16,
+            }}
+          >
+            <Typography style={{ textAlign: "center" }}>
+              Need to verify a license? Head to the platform's verifier
+            </Typography>
+          </Box>
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Button component={RouterLink} to="/verifier" variant="outlined">
+              Verify a license
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Grid>,
+  ];
+};
+
+export default HomeBanner;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,17 +1,8 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  Grid,
-  Typography,
-} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { withSnackbar } from "notistack";
-import React, { useContext, useEffect, useState } from "react";
-import { Link as RouterLink } from "react-router-dom";
-import Banner from "../../assets/images/banner/banner.jpg";
+import React, { useEffect, useState } from "react";
 import ArtworkPanel from "../../containers/ArtworkPanel/index.js";
-import { UserContext } from "../../contexts/User.js";
+import HomeBanner from "../../containers/HomeBanner";
 import { getArtwork } from "../../services/artwork.js";
 
 const initialState = {
@@ -27,7 +18,6 @@ const Home = ({ location }) => {
   const [state, setState] = useState({
     ...initialState,
   });
-  const [userStore] = useContext(UserContext);
 
   const fetchArtwork = async () => {
     try {
@@ -113,115 +103,7 @@ const Home = ({ location }) => {
       style={{ padding: 32, width: "100%", margin: 0 }}
       spacing={3}
     >
-      <Grid item xs={12} md={9}>
-        <Card
-          style={{
-            height: 360,
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            backgroundSize: "cover",
-            backgroundPosition: "center center",
-            backgroundRepeat: "no-repeat",
-            backgroundImage: `url(${Banner})`,
-            opacity: 0.3,
-          }}
-        >
-          <CardContent
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Box
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                marginBottom: 16,
-              }}
-            >
-              <Typography style={{ textAlign: "center" }}>
-                Browse, share and collect digital art the way it's supposed to
-                be done
-              </Typography>
-            </Box>
-            <Box
-              style={{
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                width: "100%",
-              }}
-            >
-              {!userStore.authenticated && (
-                <Button component={RouterLink} to="/signup" variant="outlined">
-                  Sign up
-                </Button>
-              )}
-              <Button
-                component={RouterLink}
-                to="/how_it_works"
-                variant="outlined"
-              >
-                How it works
-              </Button>
-            </Box>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} md={3}>
-        <Card
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <CardContent
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Box
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                marginBottom: 16,
-                padding: 16,
-              }}
-            >
-              <Typography style={{ textAlign: "center" }}>
-                Need to verify a license? Head to the platform's verifier
-              </Typography>
-            </Box>
-            <Box
-              style={{
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                width: "100%",
-              }}
-            >
-              <Button component={RouterLink} to="/verifier" variant="outlined">
-                Verify a license
-              </Button>
-            </Box>
-          </CardContent>
-        </Card>
-      </Grid>
+      <HomeBanner />
       <Grid item xs={12} style={{ marginTop: 32 }}>
         {state.loading || state.artwork.length ? (
           <ArtworkPanel
