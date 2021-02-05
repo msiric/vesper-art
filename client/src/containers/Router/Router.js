3fa0257d3c5c00fd35daf414b2818143ffc357cf
msiric <mario.siric@mail.com>
2021-02-05 01:56:33 +0100

    Update react router

--- client/src/containers/Router/Router.js
+++ client/src/containers/Router/Router.js
@@ -225,7 +225,7 @@ const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
           return (
             <AuthLayout>
               <Suspense fallback={<LoadingSpinner />}>
-                <Component {...props} />
+                <Component key={props.location.key} {...props} />
               </Suspense>
             </AuthLayout>
           );
@@ -258,7 +258,7 @@ const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
       return (
         <MainLayout>
           <Suspense fallback={<LoadingSpinner />}>
-            <Component {...props} />
+            <Component key={props.location.key} {...props} />
           </Suspense>
         </MainLayout>
       );
