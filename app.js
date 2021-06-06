0ce34e53f5a5d50c7d175748073b340e869df74b
msiric <mario.siric@mail.com>
2021-06-06 18:13:07 +0200

    Test new path

--- app.js
+++ app.js
@@ -96,11 +96,11 @@ app.use(
 app.use("/api", validateParams, api);
 app.use("/stripe", validateParams, stripe);
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 
 app.use((req, res, next) => {
