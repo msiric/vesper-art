f73f8a4d4c0e1e97502131ce89383f4713bbbdaf
msiric <mario.siric@mail.com>
2021-06-11 17:56:31 +0200

    Refactor OrdersToolbar

--- client/src/containers/OrdersToolbar/index.js
+++ client/src/containers/OrdersToolbar/index.js
@@ -1,32 +1,35 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
 import React from "react";
+import DropdownItems from "../../components/DropdownItems";
 import { useUserOrders } from "../../contexts/local/userOrders";
+import Grid from "../../domain/Grid";
 import globalStyles from "../../styles/global";
+import ordersToolbarStyles from "./styles";
 
 const OrderToolbar = () => {
   const display = useUserOrders((state) => state.display);
+  const loading = useUserOrders((state) => state.orders.loading);
   const changeSelection = useUserOrders((state) => state.changeSelection);
 
+  const menuItems = [
+    { value: "purchases", text: "Purchases" },
+    { value: "sales", text: "Sales" },
+  ];
+
   const globalClasses = globalStyles();
+  const classes = ordersToolbarStyles();
 
   return (
-    <FormControl
-      variant="outlined"
-      style={{ marginBottom: "12px" }}
-      className={globalClasses.dropdownOption}
-    >
-      <InputLabel id="data-display">Display</InputLabel>
-      <Select
-        labelId="data-display"
-        value={display}
-        onChange={(e) => changeSelection({ selection: e.target.value })}
-        label="Display"
-        margin="dense"
-      >
-        <MenuItem value="purchases">Purchases</MenuItem>
-        <MenuItem value="sales">Sales</MenuItem>
-      </Select>
-    </FormControl>
+    <Grid container>
+      <Grid item className={classes.wrapper}>
+        <DropdownItems
+          value={display}
+          onChange={(e) => changeSelection({ selection: e.target.value })}
+          label="Display"
+          loading={loading}
+          items={menuItems}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
