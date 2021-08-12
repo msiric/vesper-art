04ab37e438c7fd72e23a02de29ef5fc94d92de2f
msiric <mario.siric@mail.com>
2021-08-12 17:56:22 +0200

    Quickfix

--- client/src/components/LoadingSpinner/index.js
+++ client/src/components/LoadingSpinner/index.js
@@ -4,14 +4,14 @@ import Container from "../../domain/Container";
 import Grid from "../../domain/Grid";
 import loadingSpinnerStyles from "./styles";
 
-const LoadingSpinner = ({ customPadding = false }) => {
+const LoadingSpinner = ({ styles, customPadding = false }) => {
   const classes = loadingSpinnerStyles();
 
   return (
     <Container
       className={`${classes.container} ${
         customPadding && classes.customPadding
-      }`}
+      } ${styles}`}
     >
       <Grid container spacing={2}>
         <Grid item xs={12} className={classes.item}>
