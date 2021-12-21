2cbdd33fb5272c76c380165d26ea064351335f5d
Mario Širić <mario.siric@frontify.com>
2021-12-21 17:25:16 +0100

    Update notification tests

--- tests/controllers/notification.spec.js
+++ tests/controllers/notification.spec.js
@@ -4,7 +4,7 @@ import { fetchExistingNotifications } from "../../services/postgres/notification
 import { fetchUserByUsername } from "../../services/postgres/user";
 import { closeConnection, connectToDatabase } from "../../utils/database";
 import { USER_SELECTION } from "../../utils/selectors";
-import { errors } from "../../utils/statuses";
+import { errors, responses } from "../../utils/statuses";
 import { validUsers } from "../fixtures/entities";
 import { logUserIn } from "../utils/helpers";
 import { request } from "../utils/request";
@@ -20,7 +20,9 @@ let connection,
   sellerCookie,
   sellerToken,
   buyerNotifications,
-  sellerNotifications;
+  sellerNotifications,
+  readNotificationsBySeller,
+  unreadNotificationsBySeller;
 
 describe.only("Notification tests", () => {
   beforeEach(() => jest.clearAllMocks());
@@ -58,6 +60,10 @@ describe.only("Notification tests", () => {
       ]);
     ({ cookie: buyerCookie, token: buyerToken } = logUserIn(buyer));
     ({ cookie: sellerCookie, token: sellerToken } = logUserIn(seller));
+    readNotificationsBySeller = sellerNotifications.filter((item) => item.read);
+    unreadNotificationsBySeller = sellerNotifications.filter(
+      (item) => !item.read
+    );
   });
 
   afterAll(async () => {
@@ -66,15 +72,53 @@ describe.only("Notification tests", () => {
 
   describe("/api/notifications", () => {
     it("should fetch buyer notifications", async () => {
-      const res = await request(app, buyerToken).get("/api/notifications");
+      const res = await request(app, sellerToken).get("/api/notifications");
       expect(res.statusCode).toEqual(statusCodes.ok);
-      expect(res.body.notifications).toHaveLength(buyerNotifications.length);
+      expect(res.body.notifications).toHaveLength(sellerNotifications.length);
     });
 
     it("should throw an error if user is not authenticated", async () => {
       const res = await request(app).get("/api/notifications");
       expect(res.statusCode).toEqual(errors.forbiddenAccess.status);
       expect(res.body.message).toEqual(errors.forbiddenAccess.message);
+    });
+  });
+
+  describe("/api/notifications/:notificationId", () => {
+    describe("readNotification", () => {
+      it("should read a notification", async () => {
+        const res = await request(app, sellerToken).post(
+          `/api/notifications/${unreadNotificationsBySeller[0].id}`
+        );
+        expect(res.statusCode).toEqual(responses.notificationRead.status);
+        expect(res.body.message).toEqual(responses.notificationRead.message);
+      });
+
+      it("should throw an error if user is not authenticated", async () => {
+        const res = await request(app).post(
+          `/api/notifications/${unreadNotificationsBySeller[0].id}`
+        );
+        expect(res.statusCode).toEqual(errors.forbiddenAccess.status);
+        expect(res.body.message).toEqual(errors.forbiddenAccess.message);
+      });
+    });
+
+    describe("unreadNotification", () => {
+      it("should unread a notification", async () => {
+        const res = await request(app, sellerToken).delete(
+          `/api/notifications/${readNotificationsBySeller[0].id}`
+        );
+        expect(res.statusCode).toEqual(responses.notificationUnread.status);
+        expect(res.body.message).toEqual(responses.notificationUnread.message);
+      });
+
+      it("should throw an error if user is not authenticated", async () => {
+        const res = await request(app).delete(
+          `/api/notifications/${readNotificationsBySeller[0].id}`
+        );
+        expect(res.statusCode).toEqual(errors.forbiddenAccess.status);
+        expect(res.body.message).toEqual(errors.forbiddenAccess.message);
+      });
     });
   });
 });
