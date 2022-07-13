7e4c2bfafae8fbadfe0ba3d5ee4bcf5c208a48d3
Mario Širić <mario.siric@frontify.com>
2022-07-13 10:47:04 -0400

    Add artwork skeleton

--- client/src/containers/ArtworkDatatable/index.js
+++ client/src/containers/ArtworkDatatable/index.js
@@ -2,6 +2,7 @@ import {
   DeleteOutlineRounded as DeleteIcon,
   EditOutlined as EditIcon,
 } from "@material-ui/icons";
+import { datatableRowsPerPage } from "@shared/constants";
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { formatArtworkPrice, formatDate } from "../../../../common/helpers";
@@ -11,7 +12,138 @@ import { useUserStore } from "../../contexts/global/user";
 import { useUserUploads } from "../../contexts/local/userUploads";
 import Box from "../../domain/Box";
 import IconButton from "../../domain/IconButton";
-import { capitalizeWord } from "../../utils/helpers";
+import {
+  capitalizeWord,
+  determineLoadingState,
+  renderTableBody,
+} from "../../utils/helpers";
+
+const renderColumns = (loading) => [
+  {
+    name: "Id",
+    options: {
+      display: false,
+    },
+  },
+  {
+    name: "Artwork",
+    options: {
+      customBodyRender: (value) =>
+        renderTableBody(<ArtworkThumbnail source={value} />, loading, false),
+      sort: false,
+    },
+  },
+  {
+    name: "Title",
+    options: {
+      sortCompare:
+        (order) =>
+        ({ data: previous }, { data: next }) =>
+          previous.localeCompare(next, "en", {
+            numeric: true,
+          }) * (order === "asc" ? 1 : -1),
+      customBodyRender: (value) => renderTableBody(value, loading),
+    },
+  },
+  {
+    name: "Type",
+    options: {
+      customBodyRender: (value) =>
+        renderTableBody(
+          capitalizeWord({
+            value: value === "unavailable" ? "preview only" : value,
+          }) || "/",
+          loading
+        ),
+    },
+  },
+  {
+    name: "Personal license",
+    options: {
+      sortCompare:
+        (order) =>
+        ({ data: previous }, { data: next }) =>
+          (previous.amount - next.amount) * (order === "asc" ? 1 : -1),
+      customBodyRender: (value) =>
+        renderTableBody(
+          formatArtworkPrice({
+            price: value.amount,
+          }),
+          loading
+        ),
+    },
+  },
+  {
+    name: "Commercial license",
+    options: {
+      sortCompare:
+        (order) =>
+        ({ data: previous }, { data: next }) =>
+          (previous.amount - next.amount) * (order === "asc" ? 1 : -1),
+      customBodyRender: (value) =>
+        renderTableBody(
+          formatArtworkPrice({
+            price: value.amount,
+          }),
+          loading
+        ),
+    },
+  },
+  {
+    name: "Visibility",
+    options: {
+      sortCompare:
+        (order) =>
+        ({ data: previous }, { data: next }) =>
+          previous.localeCompare(next, "en", {
+            numeric: true,
+          }) * (order === "asc" ? 1 : -1),
+      customBodyRender: (value) =>
+        renderTableBody(capitalizeWord({ value }) || "/", loading),
+    },
+  },
+  {
+    name: "Date",
+    options: {
+      sortCompare:
+        (order) =>
+        ({ data: previous }, { data: next }) =>
+          (new Date(previous).getTime() - new Date(next).getTime()) *
+          (order === "asc" ? 1 : -1),
+      customBodyRender: (value) =>
+        renderTableBody(formatDate(value, "dd/MM/yy HH:mm"), loading),
+    },
+  },
+  {
+    name: "Actions",
+    options: {
+      sort: false,
+    },
+  },
+];
+
+const renderData = (uploads, actionsColumn, loading) =>
+  determineLoadingState(loading, datatableRowsPerPage, uploads).map(
+    (artwork) => [
+      artwork?.id,
+      artwork?.current?.cover.source,
+      artwork?.current?.title,
+      artwork?.current?.type,
+      {
+        amount: artwork?.current?.personal,
+        availability: artwork?.current?.availability,
+        use: artwork?.current?.use,
+      },
+      {
+        amount: artwork?.current?.commercial,
+        availability: artwork?.current?.availability,
+        use: artwork?.current?.use,
+      },
+      artwork?.visibility,
+      artwork?.current?.created,
+      actionsColumn(artwork?.id),
+    ]
+  );
 
 const ArtworkDatatable = () => {
   const userId = useUserStore((state) => state.id);
@@ -57,115 +189,8 @@ const ArtworkDatatable = () => {
   return (
     <Datatable
       title="My artwork"
-      columns={[
-        {
-          name: "Id",
-          options: {
-            display: false,
-          },
-        },
-        {
-          name: "Artwork",
-          options: {
-            customBodyRender: (value) => <ArtworkThumbnail source={value} />,
-            sort: false,
-          },
-        },
-        {
-          name: "Title",
-          options: {
-            sortCompare:
-              (order) =>
-              ({ data: previous }, { data: next }) =>
-                previous.localeCompare(next, "en", {
-                  numeric: true,
-                }) * (order === "asc" ? 1 : -1),
-          },
-        },
-        {
-          name: "Type",
-          options: {
-            customBodyRender: (value) =>
-              capitalizeWord({
-                value: value === "unavailable" ? "preview only" : value,
-              }) || "/",
-          },
-        },
-        {
-          name: "Personal license",
-          options: {
-            customBodyRender: (value) =>
-              formatArtworkPrice({
-                price: value.amount,
-              }),
-            sortCompare:
-              (order) =>
-              ({ data: previous }, { data: next }) =>
-                (previous.amount - next.amount) * (order === "asc" ? 1 : -1),
-          },
-        },
-        {
-          name: "Commercial license",
-          options: {
-            customBodyRender: (value) =>
-              formatArtworkPrice({
-                price: value.amount,
-              }),
-            sortCompare:
-              (order) =>
-              ({ data: previous }, { data: next }) =>
-                (previous.amount - next.amount) * (order === "asc" ? 1 : -1),
-          },
-        },
-        {
-          name: "Visibility",
-          options: {
-            customBodyRender: (value) => capitalizeWord({ value }) || "/",
-            sortCompare:
-              (order) =>
-              ({ data: previous }, { data: next }) =>
-                previous.localeCompare(next, "en", {
-                  numeric: true,
-                }) * (order === "asc" ? 1 : -1),
-          },
-        },
-        {
-          name: "Date",
-          options: {
-            customBodyRender: (value) => formatDate(value, "dd/MM/yy HH:mm"),
-            sortCompare:
-              (order) =>
-              ({ data: previous }, { data: next }) =>
-                (new Date(previous).getTime() - new Date(next).getTime()) *
-                (order === "asc" ? 1 : -1),
-          },
-        },
-        {
-          name: "Actions",
-          options: {
-            sort: false,
-          },
-        },
-      ]}
-      data={uploads.map((artwork) => [
-        artwork.id,
-        artwork.current.cover.source,
-        artwork.current.title,
-        artwork.current.type,
-        {
-          amount: artwork.current.personal,
-          availability: artwork.current.availability,
-          use: artwork.current.use,
-        },
-        {
-          amount: artwork.current.commercial,
-          availability: artwork.current.availability,
-          use: artwork.current.use,
-        },
-        artwork.visibility,
-        artwork.current.created,
-        actionsColumn(artwork.id),
-      ])}
+      columns={renderColumns(loading)}
+      data={renderData(uploads, actionsColumn, loading)}
       label="You have no published artwork"
       loading={loading}
       redirect="artwork"
