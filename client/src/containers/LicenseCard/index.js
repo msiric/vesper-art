5e04c3f6f01d19cdc6009b15af1c353e9cba14d8
msiric <mario.siric@mail.com>
2022-07-13 21:54:18 -0400

    Edit order tables skeleton

--- client/src/containers/LicenseCard/index.js
+++ client/src/containers/LicenseCard/index.js
@@ -2,8 +2,89 @@ import React from "react";
 import { formatArtworkPrice, formatDate } from "../../../../common/helpers";
 import Datatable from "../../components/DataTable/index";
 import { useOrderDetails } from "../../contexts/local/orderDetails";
-import { capitalizeWord } from "../../utils/helpers";
+import {
+  capitalizeWord,
+  determineLoadingState,
+  renderTableBody,
+} from "../../utils/helpers";
 import licenseCardStyles from "./styles";
+
+const renderColumns = (loading) => [
+  {
+    name: "Id",
+    options: {
+      display: false,
+      customBodyRender: (value) => renderTableBody(value, loading),
+    },
+  },
+  {
+    name: "Fingerprint",
+    options: {
+      sort: false,
+      customBodyRender: (value) => renderTableBody(value, loading),
+    },
+  },
+  {
+    name: "Type",
+    options: {
+      sort: false,
+      customBodyRender: (value) =>
+        renderTableBody(capitalizeWord({ value }) || "/", loading),
+    },
+  },
+  {
+    name: "Assignee",
+    options: {
+      sort: false,
+      customBodyRender: (value) => renderTableBody(value, loading),
+    },
+  },
+  {
+    name: "Assignee identifier",
+    options: {
+      sort: false,
+      customBodyRender: (value) => renderTableBody(value, loading),
+    },
+  },
+  {
+    name: "Assignor identifier",
+    options: {
+      sort: false,
+      customBodyRender: (value) => renderTableBody(value, loading),
+    },
+  },
+  {
+    name: "Value",
+    options: {
+      sort: false,
+      customBodyRender: (value) =>
+        renderTableBody(formatArtworkPrice({ price: value }), loading),
+    },
+  },
+  {
+    name: "Date",
+    options: {
+      sort: false,
+      customBodyRender: (value) =>
+        renderTableBody(formatDate(value, "dd/MM/yy HH:mm"), loading),
+    },
+  },
+];
+
+const renderData = (license, loading) => [
+  determineLoadingState(loading, 1, [
+    license?.id,
+    license?.fingerprint,
+    license?.type,
+    license?.usage === "business"
+      ? license?.company || "Hidden"
+      : license?.assignee || "Hidden",
+    license?.assigneeIdentifier || "Hidden",
+    license?.assignorIdentifier || "Hidden",
+    license?.price,
+    license?.created,
+  ]),
+];
 
 const LicenseCard = () => {
   const license = useOrderDetails((state) => state.order.data.license);
@@ -14,77 +95,8 @@ const LicenseCard = () => {
   return (
     <Datatable
       title="License"
-      columns={[
-        {
-          name: "Id",
-          options: {
-            display: false,
-          },
-        },
-        {
-          name: "Fingerprint",
-          options: {
-            sort: false,
-          },
-        },
-        {
-          name: "Type",
-          options: {
-            customBodyRender: (value) => capitalizeWord({ value }) || "/",
-            sort: false,
-          },
-        },
-        {
-          name: "Assignee",
-          options: {
-            sort: false,
-          },
-        },
-        {
-          name: "Assignee identifier",
-          options: {
-            sort: false,
-          },
-        },
-        {
-          name: "Assignor identifier",
-          options: {
-            sort: false,
-          },
-        },
-        {
-          name: "Value",
-          options: {
-            sort: false,
-            customBodyRender: (value) => formatArtworkPrice({ price: value }),
-          },
-        },
-        {
-          name: "Date",
-          options: {
-            sort: false,
-          },
-        },
-      ]}
-      data={[
-        ...(!loading
-          ? [
-              [
-                license.id,
-                license.fingerprint,
-                license.type,
-                license.usage === "business"
-                  ? license.company || "Hidden"
-                  : license.assignee || "Hidden",
-                license.assigneeIdentifier || "Hidden",
-                license.assignorIdentifier || "Hidden",
-                license.price,
-                license.created &&
-                  formatDate(license.created, "dd/MM/yy HH:mm"),
-              ],
-            ]
-          : []),
-      ]}
+      columns={renderColumns(loading)}
+      data={renderData(license, loading)}
       label="License not found"
       loading={loading}
       redirect=""
